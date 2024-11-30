71546452b2bdf73c2fc090f5cc60bd72d84276b1
eosdev-x <eosdev.x@gmail.com>
2024-11-29 17:05:44 -0800

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'Dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        dir: 'Dist',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  },
-    optimizeDeps: {
+  optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
